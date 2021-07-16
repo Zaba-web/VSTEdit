42516a5e449f1f8c81fd3b274880db629f46240e
Andriy <53307226+Zaba-web@users.noreply.github.com>
2021-07-17 00:01:47 +0300

    Change styles

--- src/components/FormContainer.vue
+++ src/components/FormContainer.vue
@@ -1,13 +1,16 @@
 <template>
-    <div>
+    <div class="__VSTEditor-form-container">
         <slot></slot>
     </div>
 </template>
 
-<style lang="sass" scoped>
-div
+<style lang="sass">
+.__VSTEditor-form-container
     width: 100%
     display: flex
     justify-content: space-between
     flex-wrap: wrap
+
+    & div
+        width: 45%
 </style>
