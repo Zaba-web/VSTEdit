89250be57b9003942b2454bc6c7e15132e03f82f
Andriy <53307226+Zaba-web@users.noreply.github.com>
2021-07-17 00:03:48 +0300

    Refactoring

--- src/components/VSTEditImageSettings.vue
+++ src/components/VSTEditImageSettings.vue
@@ -1,32 +1,34 @@
 <template>
     <transition name="vste-fade">
         <settings :is-visible="isVisible" :theme="theme">
-            <form-container>
-                <div>
-                    <label>Width: </label><br>
-                    <input class="__VSTEditor_input" type="text" placeholder="Enter image width" v-model="width">
-                    <br><br><br>
-                    <label>Height: </label><br>
-                    <input class="__VSTEditor_input" type="text" placeholder="Enter image height" v-model="height">
-                </div>
-                <div>
-                    <label>Alt: </label><br>
-                    <input class="__VSTEditor_input" type="text" placeholder="Enter alt" v-model="alt">
-                    <br><br><br>
-                    <button class="__VSTEditor_button" @click="saveImageData">
-                        OK
-                    </button>
-                    <button class="__VSTEditor_button" @click="close">
-                        Close
-                    </button>
-                </div>
-            </form-container>
+            <form action="">
+                <form-container>
+                    <div>
+                        <label>Width: </label><br>
+                        <input class="__VSTEditor_input" type="text" placeholder="Enter image width" v-model="attributes.width">
+                        <br><br><br>
+                        <label>Height: </label><br>
+                        <input class="__VSTEditor_input" type="text" placeholder="Enter image height" v-model="attributes.height">
+                    </div>
+                    <div>
+                        <label>Alt: </label><br>
+                        <input class="__VSTEditor_input" type="text" placeholder="Enter alt" v-model="attributes.alt">
+                        <br><br><br>
+                        <button class="__VSTEditor_button" @click="saveImageData" type="reset">
+                            OK
+                        </button>
+                        <button class="__VSTEditor_button" @click="close" type="reset">
+                            Close
+                        </button>
+                    </div>
+                </form-container>
+            </form>
         </settings>
     </transition>
 </template>
 
 <script>
-import {ref, onMounted} from 'vue'
+import {ref} from 'vue'
 
 import VSTEditSettings from '@/components/VSTEditSettings.vue'
 import FormContainer from '@/components/FormContainer.vue'
@@ -55,22 +57,15 @@ export default {
     },
     
     setup(props, context) {
-        const width = ref('')
-        const height = ref('')
-        const alt = ref('')
-
-        onMounted(()=>{
-            if(props.image == null) return ;
-
-            width.value = props.image.getAttribute('width') || 'auto'
-            height.value = props.image.getAttribute('height') || 'auto'
-            alt.value = props.image.getAttribute('alt') || ''
+        const attributes = ref({
+            width: '',
+            height: '',
+            alt: '',
         })
 
         function saveImageData() {
-            props.image.setAttribute('width', width.value)
-            props.image.setAttribute('height', height.value)
-            props.image.setAttribute('alt', alt.value)
+            for(let attr in attributes.value)
+                props.image.setAttribute(attr, attributes.value[attr])
         }
 
         function closeWindow() {
@@ -80,14 +75,8 @@ export default {
         return {
             saveImageData: saveImageData,
             close: closeWindow,
-            width: width,
-            height: height,
-            alt: alt
+            attributes: attributes
         }
     },
 }
 </script>
-
-<style lang="sass" scoped>
-
-</style>
